bee1406a66e77ab1146c92c4f36eb47b5989b0a3
NamesUnoriginal <alexjn0504@gmail.com>
2024-02-19 15:51:18 +0000

    commenting adams code cause hes dyslexic

--- XMLparser.ts
+++ XMLparser.ts
@@ -4,26 +4,26 @@ fs = require('fs');
 
 
 function XMLParserEntry(){
-    var callback;
-    let source = "C:\Users\adamh\OneDrive\Documents\GitHub\Simile-Project-Team-A\XMLExamples\CatOnMat.xml"
+    let source = "C:\Users\adamh\OneDrive\Documents\GitHub\Simile-Project-Team-A\XMLExamples\CatOnMat.xml" //source of XML file - will be  dynamic later
     let data:string[] = ReadXML(source);
     let IDs:string[];
     //find rootnode 
     data.forEach(element => {
         IDs.push(element.slice(element.indexOf('"'), element.indexOf('"',element.indexOf('"')+2))); 
+        //Pushes the text inside the ID tag of the MXcell to IDs - does this by slicing between the index of the first and second " found 
     });
     
 }
 
 function ReadXML(source: any){
     let usefulData :string[]= [];
-    fetch(source)
-        .then((res) => res.text())
+    fetch(source) //gets the file
+        .then((res) => res.text()) //waits for response and changes it to text
         .then((text) => {
-            const words = text.split('/n');
+            const words = text.split('/n'); //splits the text on new line
             words.forEach(element => {
-            if(element.search("mxCell")&&element.search("value")){
-                usefulData.push(element);
+            if(element.search("mxCell")&&element.search("value")){ //searches for a line with both mxCell and value
+                usefulData.push(element); //pushes that line to the array usefulData
             }  
             });
 
