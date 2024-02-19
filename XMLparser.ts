5a42478c2fa39657e1ecc3bcb6f36297c5b42f47
AdamHamstead <87071311+AdamHamstead@users.noreply.github.com>
2024-02-19 13:10:24 +0000

    test for cat

--- XMLparser.ts
+++ XMLparser.ts
@@ -1,1 +1,14 @@
 import { DOMParser } from 'xmldom'
+fs = require('fs');
+
+
+function XMLParserEntry(){
+    var callback;
+    let source = "C:\Users\adamh\OneDrive\Documents\GitHub\Simile-Project-Team-A\XMLExamples\CatOnMat.xml"
+    
+    const doc = new DOMParser().parseFromString(source, 'text/xml')
+
+    var nodesByName = document.getElementsByTagName('value');
+
+
+}
