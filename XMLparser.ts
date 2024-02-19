fc505e9dbd087d1cae67195c40bfa6e1ca56e8da
AdamHamstead <87071311+AdamHamstead@users.noreply.github.com>
2024-02-19 16:08:52 +0000

    done geting roots from xml files

--- XMLparser.ts
+++ XMLparser.ts
@@ -7,12 +7,20 @@ function XMLParserEntry(){
     var callback;
     let source = "C:\Users\adamh\OneDrive\Documents\GitHub\Simile-Project-Team-A\XMLExamples\CatOnMat.xml"
     let data:string[] = ReadXML(source);
-    let IDs:string[];
+    let IDs:string[]=[];
+    let targets:string[]=[];
+
     //find rootnode 
     data.forEach(element => {
         IDs.push(element.slice(element.indexOf('"'), element.indexOf('"',element.indexOf('"')+2))); 
+        if(element.includes('target="')){
+            element.lastIndexOf('target="');
+            targets.push(element.slice(element.lastIndexOf('target="'), element.indexOf('"',element.lastIndexOf('target="')+2)))
+        }
     });
-    
+    const roots = IDs.filter((element) => !targets.includes(element));
+
+
 }
 
 function ReadXML(source: any){
@@ -22,7 +30,7 @@ function ReadXML(source: any){
         .then((text) => {
             const words = text.split('/n');
             words.forEach(element => {
-            if(element.search("mxCell")&&element.search("value")){
+            if(element.includes("mxCell")&&element.includes("value")){
                 usefulData.push(element);
             }  
             });
