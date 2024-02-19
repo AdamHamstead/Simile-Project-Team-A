6c7e46905c9c2c6cfe8626b7cbcec61433b2df80
AdamHamstead <87071311+AdamHamstead@users.noreply.github.com>
2024-02-19 15:12:41 +0000

    pull from 18

--- XMLparser.ts
+++ XMLparser.ts
@@ -1,36 +1,31 @@
-import { DOMParser } from 'xmldom'
+import { Readline } from "node:readline/promises";
+
 fs = require('fs');
 
 
 function XMLParserEntry(){
     var callback;
     let source = "C:\Users\adamh\OneDrive\Documents\GitHub\Simile-Project-Team-A\XMLExamples\CatOnMat.xml"
-    let data:string = FileToString(source);
-    const doc = new DOMParser().parseFromString(data, 'text/xml')
-
-
-    var nodesByName = document.getElementsByTagName('value');
-    
-
-
+    let data = ReadXML(source);
+    //find rootnode 
+    data.forEach(element => {
+        
+    });
 }
 
-function FileToString(source: any){
-    const fs = require('fs');
-    // Open the file stream
-        const stream = fs.createReadStream(source);
-    
-    // Create a string variable to store the data
-        let data = '';
-    
-    // Listen for the 'data' event and append the data to the string variable
-        stream.on('data', (chunk: { toString: () => string; }) => {
-            data += chunk.toString();
-        });
-    // Listen for the 'end' event and log the final string value
-    stream.on('end', () => {
-      console.log(data);
-    });
-    return data;
+async function ReadXML(source: any){
+    let usefulData :any[]= [];
+    fetch(source)
+        .then((res) => res.text())
+        .then((text) => {
+            const words = text.split('/n');
+            words.forEach(element => {
+            if(element.search("mxCell")&&element.search("value")){
+                usefulData.push(usefulData);
+            }  
+            });
 
+        })
+    .catch((e) => console.error(e));
+    return usefulData;
 }
