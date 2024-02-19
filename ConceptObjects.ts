4c1dfbf7c41111f314b42d3a6720640f610f6eae
NamesUnoriginal <alexjn0504@gmail.com>
2024-02-19 14:34:26 +0000

    Added explanation for objects

--- ConceptObjects.ts
+++ ConceptObjects.ts
@@ -1,3 +1,20 @@
+/* 
+    These are mainly to replace the triples. Instead of a 2d array storing the values, we can have the objects do it.
+
+    Source Concept -> relation(s) -> Target Concept
+
+    Concept - similar to a node in a graph. Basically stores all data about the concept itself (e.g Cat: Bumbles). They also
+    store an array of Relation objects which connect them to other concepts. Generally most functions will probably go into
+    this Obejct.
+
+    Relation - similar to an edge in a graph. Stores the source concept, target concept (both concept objects and the value of that relation 
+    (e.g sits on). Don't expect many functions to be included in these relations. 
+
+    Add any functions you think will be useful and explain why you are using them in comments as best you can. We want these to basically 
+    replace most helper functions and global variables (but it wont get them all).
+
+*/
+
 class Concept
 {
     private value: string;
