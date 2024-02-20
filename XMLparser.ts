4ce9f8f81f23da5776d6fc56f89f3fc13873f439
AdamHamstead <87071311+AdamHamstead@users.noreply.github.com>
2024-02-20 14:00:51 +0000

    first full virsion defiatly broken ik i cant spell shut

--- XMLparser.ts
+++ XMLparser.ts
@@ -7,9 +7,10 @@ import { Relation } from "./ConceptObjects";
 function XMLParserEntry(){
     let source = "C:\Users\adamh\OneDrive\Documents\GitHub\Simile-Project-Team-A\XMLExamples\CatOnMat.xml" //source of XML file - will be  dynamic later
     let data:string[] = ReadXML(source);
-    const roots = FindRootNode(data);
-    roots.forEach(element => {
-        CreateNodes(element,data);
+    const rootids = FindRootNode(data);
+    let roots = new Array<Concept>;
+    rootids.forEach(element => {
+        roots.push(CreateConcept(element,data) as Concept);
     });
 }
 
@@ -37,12 +38,45 @@ function FindRootNode(data:string[]){
         IDs.push(element.slice(element.indexOf('"'), element.indexOf('"',element.indexOf('"')+2))); 
         //Pushes the text inside the ID tag of the MXcell to IDs - does this by slicing between the index of the first and second " found 
         if(element.includes('target="')){
-            targets.push(element.slice(element.lastIndexOf('target="'), element.indexOf('"',element.lastIndexOf('target="')+2)))
+            targets.push(element.slice(element.lastIndexOf('target="'), element.indexOf('"',element.lastIndexOf('target="')+2)));
         }
     });
     return IDs.filter((element) => !targets.includes(element));
 }
 
-function CreateNodes(node:string ,data:string[]){
+function CreateConcept(id:string ,data:string[]){
+    let node = new Concept(SearchForValue(id,data));
+    let endnode = 0;
+    data.forEach(element => {
+        if(element.includes('source="'+id+'"')) {
+            endnode++;
+            let IDofTarget = element.slice(element.lastIndexOf('target="'), element.indexOf('"',element.lastIndexOf('target=""')+1));
+            let temp = SearchForNode(IDofTarget ,data)
+            CreateRelation(node,temp as string,data);
 
+        }
+    });
+    if(!endnode){
+        return node;
+    }
+}
+function CreateRelation(source:Concept,mxCell:string,data:string[]){
+    //mxcell is target
+    //need value 
+    //create the target if id does not exists
+    mxCell.slice(mxCell.lastIndexOf('value="'), mxCell.indexOf('"',mxCell.lastIndexOf('value="')+1));//gets value 
+    let target = CreateConcept(mxCell.slice(mxCell.lastIndexOf('value="'), mxCell.indexOf('"',mxCell.lastIndexOf('value="')+1)),data);
+    let relation = new Relation(source,target as Concept,mxCell.slice(mxCell.lastIndexOf('value="'), mxCell.indexOf('"',mxCell.lastIndexOf('value="')+1)));
+    return 1;
+}
+function SearchForValue (str:string, strArray:string[]) {
+    for (var j=0; j<strArray.length; j++) {
+        if (strArray[j].match(str)) break;
+    }
+    return strArray[j].slice(strArray[j].lastIndexOf('value="'), strArray[j].indexOf('"',strArray[j].lastIndexOf('value="')+1));
+}
+function SearchForNode (str:string, strArray:string[]) {
+    for (var j=0; j<strArray.length; j++) {
+        if (strArray[j].match(str)) return strArray[j];
+    }
 }
