e7c54594012574b2d88ccfdedefd35c4d99abe3b
NamesUnoriginal <alexjn0504@gmail.com>
2024-02-21 13:32:56 +0000

    un-typed xml parser

--- XMLparser.js
+++ XMLparser.js
@@ -6,30 +6,31 @@ import { Concept } from './ConceptObjects';
 import { Relation } from "./ConceptObjects";
 
 XMLParserEntry();
-function XMLParserEntry(){  
-    let source = fs.readFileSync('foo.txt','utf8'); //source of XML file - will be  dynamic later
-    let data:string[] = ReadXML(source);
-    const rootids = FindRootNode(data);
-    let roots = new Array<Concept>;
-    rootids.forEach(element => {
-        roots.push(CreateConcept(element,data) as Concept);
-    });
+function XMLParserEntry(){
+    console.log("kys")  
+    // let source = fs.readFileSync('foo.txt','utf8'); //source of XML file - will be  dynamic later
+    // let data = ReadXML(source);
+    // const rootids = FindRootNode(data);
+    // let roots = [];
+    // rootids.forEach(element => {
+    //     roots.push(CreateConcept(element,data));
+    // });
 }
 
-function ReadXML(source: any){
-    let usefulData :string[]= [];
-            const words = source.split('/n'); //splits the text on new line
-            words.forEach(element => {
-            if(element.includes("mxCell")&&element.includes("value")){
-                usefulData.push(element);
-            }  
-            }
+function ReadXML(source){
+    let usefulData = [];
+            // const words = source.split('/n'); //splits the text on new line
+            // words.forEach(element => {
+            // if(element.includes("mxCell")&&element.includes("value")){
+            //     usefulData.push(element);
+            // }  
+            // }
     return usefulData;
 }
-function FindRootNode(data:string[]){
+function FindRootNode(data){
 
-    let IDs:string[]=[];
-    let targets:string[]=[];
+    let IDs=[];
+    let targets=[];
     data.forEach(element => {
         IDs.push(element.slice(element.indexOf('"'), element.indexOf('"',element.indexOf('"')+2))); 
         //Pushes the text inside the ID tag of the MXcell to IDs - does this by slicing between the index of the first and second " found 
@@ -40,7 +41,7 @@ function FindRootNode(data:string[]){
     return IDs.filter((element) => !targets.includes(element));
 }
 
-function CreateConcept(id:string ,data:string[]){
+function CreateConcept(id ,data){
     let node = new Concept(SearchForValue(id,data));
     let endnode = 0;
     data.forEach(element => {
@@ -48,7 +49,7 @@ function CreateConcept(id:string ,data:string[]){
             endnode++;
             let IDofTarget = element.slice(element.lastIndexOf('target="'), element.indexOf('"',element.lastIndexOf('target=""')+1));
             let temp = SearchForNode(IDofTarget ,data)
-            CreateRelation(node,temp as string,data);
+            CreateRelation(node,temp,data);
 
         }
     });
@@ -56,22 +57,22 @@ function CreateConcept(id:string ,data:string[]){
         return node;
     }
 }
-function CreateRelation(source:Concept,mxCell:string,data:string[]){
+function CreateRelation(source,mxCell,data){
     //mxcell is target
     //need value 
     //create the target if id does not exists
     mxCell.slice(mxCell.lastIndexOf('value="'), mxCell.indexOf('"',mxCell.lastIndexOf('value="')+1));//gets value 
     let target = CreateConcept(mxCell.slice(mxCell.lastIndexOf('value="'), mxCell.indexOf('"',mxCell.lastIndexOf('value="')+1)),data);
-    let relation = new Relation(source,target as Concept,mxCell.slice(mxCell.lastIndexOf('value="'), mxCell.indexOf('"',mxCell.lastIndexOf('value="')+1)));
+    let relation = new Relation(source,target ,mxCell.slice(mxCell.lastIndexOf('value="'), mxCell.indexOf('"',mxCell.lastIndexOf('value="')+1)));
     return 1;
 }
-function SearchForValue (str:string, strArray:string[]) {
+function SearchForValue (str, strArray) {
     for (var j=0; j<strArray.length; j++) {
         if (strArray[j].match(str)) break;
     }
     return strArray[j].slice(strArray[j].lastIndexOf('value="'), strArray[j].indexOf('"',strArray[j].lastIndexOf('value="')+1));
 }
-function SearchForNode (str:string, strArray:string[]) {
+function SearchForNode (str, strArray) {
     for (var j=0; j<strArray.length; j++) {
         if (strArray[j].match(str)) return strArray[j];
     }
