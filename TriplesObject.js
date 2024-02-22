77860fb40193871a5ce70a3672225e78c2068ede
Daniel Mozafari <116290928+Dn1San@users.noreply.github.com>
2024-02-22 14:55:41 +0000

    Triples Object

--- TriplesObject.js
+++ TriplesObject.js
@@ -1,17 +1,42 @@
 class Triples{
-    constructor(numberOfConcepts, referents, triple, numberOfTriples){
-        this.numberOfConcepts = numberOfConcepts;
-        this.referents = referents;
-        this.triple = triple;
-        this.numberOfTriples = numberOfTriples;
-    }
-
+    numberOfConcepts = 0;
+    numberOfTriples = 0;
+    numberOfRepeats = 0;
     referent = ""; // CG concept referent
     numSourceConcInRelation = 0;
     sourceConceptIndex = [];
     SOURCE = 0;		
     RELATION = 1;		
     TARGET = 2;
+    ATTRIBUTE = 0;
+    OBJECT = 1;
+    TIMES = 2;
+
+    constructor(concepts, referents, triple, repeats, relationLabels){
+        this.concepts = concepts;
+        this.referents = referents;
+        this.triple = triple;
+        this.repeats = repeats;
+        this.relationLabels = relationLabels;
+    }
+
+    setNumberOfConcepts(){
+        this.concepts.forEach(() =>{ // Loop through concepts
+            this.numberOfConcepts++; // Count number of concepts
+        });
+    }
+
+    setNumberOfTriples(){
+        this.triple.forEach(() =>{ // Loop through triples
+            this.numberOfTriples++; // Count number of triples
+        });
+    }
+
+    setNumberOfRepeats(){
+        this.repeats.forEach(() =>{
+            this.numberOfRepeats++;
+        });
+    }
 
     makeTriples(rel, ref_list){
         for(let i = 0; i < ref_list.length; i++) {
@@ -54,6 +79,31 @@ class Triples{
         }
 
         return this.triple;
+    }
+
+    triples_to_binaries(rfname){
+        let path = Array.from(Array(100000), () => new Array(2).fill(0)); //Look at this later**
+        //let path:number[100000][]; //to record each transitive path through triples
+        let pathSize = 0;
+    
+        //converts (source,relation,target) triples to ((source^relation),target) binary relations
+        for(let attribute = 0; attribute<this.numberOfTriples; attribute++){
+            let target = this.triple[attribute][this.TARGET];
+            let relation = this.triple[attribute][this.RELATION];
+            let source = this.triple[attribute][this.SOURCE];
+            add_binary(attribute, source, relation, target, path, pathSize, rfname);
+        }
+    
+        //output repeats
+        for(let i = 0; i<this.numberOfRepeats; i++){
+            let source = this.triple[this.repeats[i][this.ATTRIBUTE]][this.SOURCE];
+            let relation = this.triple[this.repeats[i][this.ATTRIBUTE]][this.RELATION];
+            let output = this.repeats[i][this.OBJECT];
+            let target = this.triple[this.repeats[i][this.ATTRIBUTE]][this.TARGET];
+            let times = this.repeats[i][this.TIMES] + 1;
+            console.log("\n\n" + times + " direct pathways from \"" + this.concepts[source] + " - " + this.relationLabels[relation] + " - " + this.concepts[target] + "\" to \"" + this.concepts[output] + "\"");
+            fs.appendFileSync(rfname, "\n\n " + times + " direct pathways from \"" + this.concepts[source] + " - " + this.relationLabels[relation] + " - " + this.concepts[target] + "\" to \"" + this.concepts[output] + "\"");
+        }
     }
     
 }
