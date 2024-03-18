04db163bd93460ece54ab17cfba934799c5949a9
AdamHamstead <87071311+AdamHamstead@users.noreply.github.com>
2024-03-18 16:17:55 +0000

    stuff

--- CGFCA.js
+++ CGFCA.js
@@ -1,4 +1,5 @@
 document.getElementById("CGFCA").addEventListener("click", myFunction);
+document.getElementById("clickMe").addEventListener("click",getMXCELL);
 
 const drawio = require('./drawio-desktop\src\main\electron.js');
 
@@ -7,8 +8,12 @@ function myFunction(){
 
     var path = document.getElementById("file").value //file path of selected file
     //error checking for correct file type
-
-
     drawio.myFunction();
 }
-
+function getMXCELL(){
+    console.log("test")
+    const element = document.getElementsByName("g");
+    document.getElementById("demo").innerHTML = 'The first paragraph (index 0) inside "main" is: ' + element;
+    let t,e;
+    console.log( document.getElementById("drawio-desktop").contentWindow.EditorUi.prototype.getFileData(true,null,null,null,true,false,null,null,null,true))
+  }
